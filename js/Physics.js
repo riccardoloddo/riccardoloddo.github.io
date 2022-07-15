2cabae7bd69488b3e3bfac46c0b764a2b4c32c3a
riccardoloddo <109277791+riccardoloddo@users.noreply.github.com>
2022-07-15 20:12:17 +0200

    Changed color in AboutMe

---
+++ js/Physics.js
@@ -0,0 +1,6 @@
+const canvas = document.getElementById("canvas1");
+const ctx = canvas.getContext('2d');
+canvas.width = window.innerWidth;
+canvas.height = window.innerHeight;
+
+let particlesArray min 4.44 Particles JS Effect with Pure Vanilla JavaScript | Animated Background Tutorial 
