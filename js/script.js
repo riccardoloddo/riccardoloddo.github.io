6f98746b4218b58e51902afe26cfdf94c11fb546
riccardoloddo <109277791+riccardoloddo@users.noreply.github.com>
2022-07-20 19:22:42 +0200

    Totaly change

---
+++ js/script.js
@@ -0,0 +1,7 @@
+/* =========================== typing animation ================================*/
+var typed = new Typed(".typing", {
+  strings:["","student", "physicist", "climber", "pianist"],
+  typeSpeed:100,
+  BackSpeed:60,
+  loop:true
+})
