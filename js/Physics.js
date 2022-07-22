8d5fc1e2e07bf7b513c7d699becb4a41c616ba86
riccardoloddo <109277791+riccardoloddo@users.noreply.github.com>
2022-07-22 19:15:23 +0200

    Particles

--- js/Physics.js
+++ js/Physics.js
@@ -10,10 +10,9 @@ let mouse = {
   x: null,
   y: null,
   radius: (canvas.height/80) * (canvas.width/80)
-};
+}
 
-window.addEventListener('mousemove',
-  function(event) {
+window.addEventListener("mousemove",function(event) {
     mouse.x = event.x;
     mouse.y = event.y;
   }
@@ -34,7 +33,7 @@ class Particle {
   draw() {
     ctx.beginPath();
     ctx.arc(this.x, this.y, this.size, 0, Math.PI * 2, false);
-    ctx.fillStyle = '#8C5523';
+    ctx.fillStyle = '#fff';
     ctx.fill();
   }
  // check particle position and mouse position, move the particles and draw the particles
@@ -53,16 +52,16 @@ class Particle {
     let dy = mouse.y - this.y;
     let distance = Math.sqrt(dx*dx + dy*dy);
     if(distance < mouse.radius + this.size){
-      if(mouse.x < this.x && this.x < canvas.width - this.size * 10){
+      if(mouse.x < this.x && this.x < canvas.width - this.size * 10) {
         this.x += 10;
       }
-      if(mouse.x > this.x && this.x > this.size * 10){
+      if(mouse.x > this.x && this.x > this.size * 10) {
         this.x -= 10;
       }
-      if(mouse.y < this.y && this.y < canvas.height - this.size * 10){
+      if(mouse.y < this.y && this.y < canvas.height - this.size * 10) {
         this.y += 10;
       }
-      if(mouse.y > this.y && this.y > this.size * 10){
+      if(mouse.y > this.y && this.y > this.size * 10) {
         this.y -= 10;
       }
     }
@@ -78,38 +77,30 @@ class Particle {
 function init() {
   particlesArray = [];
   let numberOfParticles = (canvas.height * canvas.width) / 9000;
-  for(let i = 0; i < numberOfParticles; i++) {
+  for(let i = 0; i < numberOfParticles*2; i++) { /*Per mettere piu particelle aumetare numberOfParticles*/
     let size = (Math.random() * 5) + 1;
     let x = (Math.random() * ((innerWidth - size * 2) - (size * 2)) + size * 2);
     let y = (Math.random() * ((innerHeight - size * 2) - (size * 2)) + size * 2);
     let directionX = (Math.random() * 5) - 2.5;
     let directionY = (Math.random() * 5) - 2.5;
-    let color = '#000';
+    let color = '#8C5523';
 
     particlesArray.push(new Particle(x, y, directionX, directionY, size, color));
   }
 };
 
-// animation loop
-function animate() {
-  requestAnimationFrame(animate);
-  ctx.clearRect(0, 0, innerWidth, innerHeight);
-
-  for(let i = 0; i < particlesArray.lenght; i++) {
-    particlesArray[i].update();
-  }
-};
-
 //check if particles are close enought to draw line between them
-/*function connect(){
-  for(let a = 0; a < particlesArray.lenght; a++){
-    for(let b = a; b < particlesArray.lenght; b++){
+function connect(){
+  let opacityValue = 1;
+  for(let a = 0; a < particlesArray.length; a++){
+    for(let b = a; b < particlesArray.length; b++){
       let distance = ((particlesArray[a].x - particlesArray[b].x)
       * (particlesArray[a].x - particlesArray[b].x))
       + ((particlesArray[a].y - particlesArray[b].y)
       * (particlesArray[a].y - particlesArray[b].y));
       if(distance < (canvas.width/7)*(canvas.height/7)) {
-        ctx.strokeStyle = 'rgba(140, 85, 31, 1)';
+        opacityValue = 1 - (distance/20000);
+        ctx.strokeStyle = 'rgba(225,225,225,' + opacityValue +')';
         ctx.lineWidth = 1;
         ctx.beginPath();
         ctx.moveTo(particlesArray[a].x, particlesArray[a].y);
@@ -118,7 +109,34 @@ function animate() {
       }
     }
   }
-};*/
+};
+
+// animation loop
+function animate() {
+  requestAnimationFrame(animate);
+  ctx.clearRect(0, 0, innerWidth, innerHeight);
+
+  for(let i = 0; i < particlesArray.length; i++) {
+    particlesArray[i].update();
+  }
+  connect();
+};
+
+window.addEventListener('resize', function()
+{
+  canvas.width = innerWidth;
+  canvas.height = innerHeight;
+  mouse.radius = ((canvas.height/80) * (canvas.height/80));
+  init();
+})
+
+/* ============= MOUSE OUT EVENT ==============*/
+window.addEventListener('mouseout', function()
+{
+  mouse.x = undefined;
+  mouse.x = undefined;
+})
+
 
 init();
 animate();
